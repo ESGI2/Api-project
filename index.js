ecc53cd143617621c4dc52f81bf972fb85597ba8
Enzo <enzo.moy94@gmail.com>
2023-12-08 11:30:43 +0100

    Setup Project

---
+++ index.js
@@ -0,0 +1,21 @@
+// Initialition de l'api express
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv').config();
+const connection = require('./backend/config/db');
+
+// Connexion à la base de données
+const db = connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connecté à la base de données MySQL!');
+});
+
+// Lancement du serveur
+app.listen(8000, () => {
+  console.log('Le serveur a bien démarré au port 8000');
+});
+
+// Middleware pour parser les requêtes POST
+var bodyParser = require('body-parser')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
