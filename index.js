313925dd8012d69876defbf9ebdf11338cf91ab7
Sitron808 <kevinsureshe@gmail.com>
2023-12-15 02:56:57 +0100

    reservationBeforeAppart

--- index.js
+++ index.js
@@ -1,21 +1,92 @@
-// Initialition de l'api express
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv').config();
-const connection = require('./backend/config/db');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const { Op } = require('sequelize');
+const { sequelize } = require('./models/reservation.model.js');
+const { Reservation } = require('./models/reservation.model');
+const reservationController = require('./backend/controllers/reservation.controller');
 
-// Connexion à la base de données
-const db = connection.connect((err) => {
-  if (err) throw err;
-  console.log('Connecté à la base de données MySQL!');
+
+const syncDatabase = async () => {
+  try {
+    await sequelize.sync();
+    console.log('Base de données synchronisée');
+  } catch (error) {
+    console.error('Erreur lors de la synchronisation de la base de données:', error);
+  }
+};
+
+
+// Middleware pour le corps de la requête
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Middleware pour vérifier le jeton d'authentification
+function authenticateToken(req, res, next) {
+  const token = req.headers['authorization'];
+  if (!token) return res.status(401).json({ error: 'Accès non autorisé' });
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return res.status(403).json({ error: 'Le jeton n\'est pas valide' });
+
+    req.user = user;
+    next();
+  });
+}
+
+// Routes
+app.get('/api/user/:userId', authenticateToken, async (req, res) => {
+  const { userId } = req.params;
+
+  try {
+    const user = await getUserById(userId);
+    if (!user) {
+      return res.status(404).json({ error: 'Utilisateur non trouvé' });
+    }
+
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erreur serveur' });
+  }
+});
+
+// Routes Res
+app.get('/api/reservation', reservationController.getAllReservations);
+app.get('/api/reservation/:reservationId', reservationController.getReservationById);
+app.post('/api/reservation', authenticateToken, reservationController.createReservation);
+
+app.get('/', (req, res) => {
+  res.send('Bienvenue sur l\'API !');
 });
 
 // Lancement du serveur
-app.listen(8000, () => {
-  console.log('Le serveur a bien démarré au port 8000');
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`Le serveur a bien démarré au port ${port}`);
 });
 
-// Middleware pour parser les requêtes POST
-var bodyParser = require('body-parser')
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// Fonction pour récupérer les informations de l'utilisateur
+async function getUserById(userId) {
+  try {
+    //Sequelize
+    const user = await User.findByPk(userId);
+
+    if (!user) {
+      return null;
+    }
+    const formattedUser = {
+      id: user.id,
+      nom: user.nom,
+      role: user.role,
+    };
+
+    return formattedUser;
+  } catch (error) {
+    console.error(error);
+    throw new Error('Erreur lors de la récupération des informations de l\'utilisateur.');
+  }
+}
+
