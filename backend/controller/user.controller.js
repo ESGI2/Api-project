863d3d7dd45db2d75c2d439150f55db7ad384c56
Enzo <enzo.moy94@gmail.com>
2023-12-20 22:16:28 +0100

    add user route

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,15 +1,31 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../config/db");
+const UserServices = require('../services/user.service');
+const AppartementServices = require('../services/appartement.service');
+const ReservationServices = require('../services/reservation.service');
 
 
-module.exports.getAllUser = (req, res) => {
+module.exports.getAllUser = async (req, res) => {
     try {
-        db.query("SELECT * FROM user", (err, result) => {
-            if (err) throw err;
-            res.status(200).send(result);
-        });
-    } catch (err) {
-        db.errorHandler(res, err);
+        const user = await UserServices.getAllUser();
+        res.status(200).json(user);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+}
+
+module.exports.getUserById = async (req, res) => {
+    try {
+        const user = await UserServices.getUserById(req.params.id);
+        if (user) {
+            res.status(200).json(user);
+        } else {
+            res.status(404).json({ message: 'User not found' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
     }
 }
